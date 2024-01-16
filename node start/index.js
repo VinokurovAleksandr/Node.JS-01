a697b90c1a83ded32dc8c535b3a53403f155225a
Aleksandr Vinokurov <98280490+VinokurovAleksandr@users.noreply.github.com>
2024-01-16 22:07:40 +0200

    testing

--- node start/index.js
+++ node start/index.js
@@ -3,26 +3,65 @@
 // console.log(users);
 
 
-// const {admins} = require("./user.mjs");
+// const {admins} = require("./user");
 // console.log(admins);
 
-
-// const  {getCurrentMonth} = require('./date');
-// const currentMonth = getCurrentMonth();
-// console.log(currentMonth);
+// import getCurrentMonth from './date/index.js'
+// // const  {getCurrentMonth} = require('./date');
+// // const currentMonth = getCurrentMonth();
+// console.log(getCurrentMonth);
 
 // const currentMonth = require('./date/index.mjs').getCurrentMonth();
 // console.log(currentMonth);
 
+// const fs = require("fs/promises");
+
+
+// import moduleName from './work-with-files/files/file.txt'
+
+// const  contactsPath  = async({filePath, action, data}) => {
+//     switch(action) {
+//         case "read":
+//             const text = await fs.readFile(filePath, 'utf-8');
+//             console.log(text);
+//             break;
+            
+//     }
+// }
+// contactsPath({filePath: './db/file.txt', action: "read"})
+
+
+// const fs = require("fs/promises");
+
+// fs.readFile("./files/file.txt", )
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error)
+//     )
+
 const fs = require("fs/promises");
 
-const  contactsPath  = async({filePath, action, data}) => {
-    switch(action) {
-        case "read":
-            const text = await fs.readFile(filePath, 'utf-8');
+const  fileOperation = async({filePath, action,data}) => {
+    switch (action) {
+        case 'read':
+            const text = await fs.readFile(filePath, 'utf-8')
             console.log(text);
-            break;
             
+            // const data = await fs.readFile(filePath);
+            // const text = data.toString()
+            // console.log(text);
+            
+            break;
+            case 'add':
+               await fs.appendFile(filePath,data);
+                break;
+                case 'replace':
+                   const result =  await fs.writeFile(filePath,data);
+                   console.log(result);
+                   
+                    break;
     }
 }
-contactsPath({filePath: './db/file.txt', action: "read"})
+
+// fileOperation({filePath: "./work-with-files/files/file.txt", action: "add", data: '\n Pyanashok'})
+// fileOperation({filePath: "./work-with-files/files/file.txt", action: "replace", data: '\n dfdfdfd'})
+fileOperation({filePath: "./work-with-files/files/file2.txt", action: "replace", data: '\n dfdfdfd'})
